4bd253028045a6ea6e7cf2ade478c5c964e9bf93
prosperous1093@ <prosperous1093@gmail.com>
2024-10-12 22:02:36 +0700

    Add new Script for Channel

---
+++ Assets/VuongLai/TicTacToe/Scripts/ScriptableObject/Channels/V_ReturnIntegerListChannel.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace V_TicTacToe
+{
+    [CreateAssetMenu(fileName = "V_ReturnIntegerListChannel", menuName = "ScriptableObject/Channel/V_ReturnIntegerListChannel")]
+    public class V_ReturnIntegerListChannel : ScriptableObject
+    {
+        private Func<int, List<int>> _value;
+
+        public void AddListener(Func<int, List<int>> func)
+        {
+            _value += func;
+        }
+
+        public void RemoveListener(Func<int, List<int>> func)
+        {
+            _value -= func;
+        }
+
+        public List<int> RunChannel(int inputValue)
+        {
+            return _value?.Invoke(inputValue);
+        }
+    }
+}
---
+++ Assets/VuongLai/TicTacToe/Scripts/ScriptableObject/Channels/V_ReturnVector2Channel.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace V_TicTacToe
+{
+    [CreateAssetMenu(fileName = "V_ReturnVector2Channel", menuName = "ScriptableObject/Channel/V_ReturnVector2Channel")]
+    public class V_ReturnVector2Channel : ScriptableObject
+    {
+        private Func<int, Vector3> _value;
+
+        public void AddListener(Func<int,Vector3> func)
+        {
+            _value += func;
+        }
+
+        public void Remove(Func<int, Vector3> func)
+        {
+            _value -= func;
+        }
+
+        public Vector3? RunChannel(int inputValue)
+        {
+            Vector3? outputValue= _value?.Invoke(inputValue);
+
+            return outputValue;
+        }
+    }
+}
